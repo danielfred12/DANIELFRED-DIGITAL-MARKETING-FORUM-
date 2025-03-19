2f8fee76fa363cd966e0891a7738b927b0200133
danielfred12 <enokeladaniel92@gmail.com>
2025-03-19 07:16:16 +0100

    Add files via upload

---
+++ File - script.js
@@ -0,0 +1,9 @@
+// Open Terms Modal
+function openTermsModal() {
+  document.getElementById("termsModal").style.display = "block";
+}
+
+// Close Terms Modal
+function closeTermsModal() {
+  document.getElementById("termsModal").style.display = "none";
+}
